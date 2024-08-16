aea3aee43cac8074edeaa54fb3a51e0170a7fde9
Debargha Mondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-08-16 10:19:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+}
+Car.prototype.getMakeModel = function(){
+	return this.make + " " + this.model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this, make, model);
+	this.topSpeed = topSpeed;
+}
+
+SportsCar.prototype = 
+	Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function(){
+	return this.topSpeed;
+}
+
+const myCar = new SportsCar('Tesla', 'Model S', 250);
+console.log(myCar.getMakeModel());
+console.log(myCar.getTopSpeed()); 
+
 
 // Do not change the code below
 window.Car = Car;
